2d42b202dc00031b32b14b0119fb014a08d5b6b6
SeoungGeunYeol <swaq11@naver.com>
2023-11-09 17:12:46 +0900

    세션 3 완료

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,13 +1,19 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
@@ -21,9 +27,5 @@ class MemberServiceTest {
         //then
         Assertions.assertThat(member).isEqualTo(findmember);
 
-    }
-
-    @Test
-    void findMember() {
     }
 }
